f19f5c3949ab993b9a54a4e34a2617198297ce67
Itamar <itamarh@microsoft.com>
2024-03-13 20:03:12 +0200

    First commit - basic functionality

--- variables.tf
+++ variables.tf
@@ -24,11 +24,8 @@ variable "name" {
   type        = string
   description = "The name of the this resource."
   validation {
-    condition     = can(regex("TODO determine REGEX", var.name))
-    error_message = "The name must be TODO."
-    # TODO remove the example below once complete:
-    #condition     = can(regex("^[a-z0-9]{5,50}$", var.name))
-    #error_message = "The name must be between 5 and 50 characters long and can only contain lowercase letters and numbers."
+    condition     = can(regex("^[^#]+$", var.name))
+    error_message = "The name must be at least 1 characters long."
   }
 }
 
@@ -115,57 +112,6 @@ variable "managed_identities" {
   default     = {}
 }
 
-variable "private_endpoints" {
-  type = map(object({
-    name = optional(string, null)
-    role_assignments = optional(map(object({
-      role_definition_id_or_name             = string
-      principal_id                           = string
-      description                            = optional(string, null)
-      skip_service_principal_aad_check       = optional(bool, false)
-      condition                              = optional(string, null)
-      condition_version                      = optional(string, null)
-      delegated_managed_identity_resource_id = optional(string, null)
-    })), {})
-    lock = optional(object({
-      name = optional(string, null)
-      kind = optional(string, "None")
-    }), {})
-    tags                                    = optional(map(any), null)
-    subnet_resource_id                      = string
-    private_dns_zone_group_name             = optional(string, "default")
-    private_dns_zone_resource_ids           = optional(set(string), [])
-    application_security_group_associations = optional(map(string), {})
-    private_service_connection_name         = optional(string, null)
-    network_interface_name                  = optional(string, null)
-    location                                = optional(string, null)
-    resource_group_name                     = optional(string, null)
-    ip_configurations = optional(map(object({
-      name               = string
-      private_ip_address = string
-    })), {})
-  }))
-  default     = {}
-  description = <<DESCRIPTION
-A map of private endpoints to create on this resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-
-- `name` - (Optional) The name of the private endpoint. One will be generated if not set.
-- `role_assignments` - (Optional) A map of role assignments to create on the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time. See `var.role_assignments` for more information.
-- `lock` - (Optional) The lock level to apply to the private endpoint. Default is `None`. Possible values are `None`, `CanNotDelete`, and `ReadOnly`.
-- `tags` - (Optional) A mapping of tags to assign to the private endpoint.
-- `subnet_resource_id` - The resource ID of the subnet to deploy the private endpoint in.
-- `private_dns_zone_group_name` - (Optional) The name of the private DNS zone group. One will be generated if not set.
-- `private_dns_zone_resource_ids` - (Optional) A set of resource IDs of private DNS zones to associate with the private endpoint. If not set, no zone groups will be created and the private endpoint will not be associated with any private DNS zones. DNS records must be managed external to this module.
-- `application_security_group_resource_ids` - (Optional) A map of resource IDs of application security groups to associate with the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-- `private_service_connection_name` - (Optional) The name of the private service connection. One will be generated if not set.
-- `network_interface_name` - (Optional) The name of the network interface. One will be generated if not set.
-- `location` - (Optional) The Azure location where the resources will be deployed. Defaults to the location of the resource group.
-- `resource_group_name` - (Optional) The resource group where the resources will be deployed. Defaults to the resource group of this resource.
-- `ip_configurations` - (Optional) A map of IP configurations to create on the private endpoint. If not specified the platform will create one. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-  - `name` - The name of the IP configuration.
-  - `private_ip_address` - The private IP address of the IP configuration.
-DESCRIPTION
-}
 
 variable "role_assignments" {
   type = map(object({
@@ -199,3 +145,81 @@ variable "tags" {
   default     = {}
 }
 
+variable "virtual_network_id" {
+  type        = string
+  description = "The ID of the virtual network to deploy the private DNS resolver in."
+}
+
+variable "inbound_endpoints" {
+  type = map(string)
+  default     = {}
+  description = <<DESCRIPTION
+A map of subnets for which inbound endpoint will be created.
+- The key is the name for the endpoint
+- The value is the subnet ID
+DESCRIPTION
+}
+
+# variable "outbound_endpoints" {
+#   type = map(string)
+#   default     = {}
+#   description = <<DESCRIPTION
+# A map of outbound endpoints to create on this resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+# - The key is the name for the endpoint
+# - The value is the subnet ID
+# DESCRIPTION
+# }
+
+variable "outbound_endpoints" {
+  type = map(object({
+    name = string
+    subnet_id = string
+    forwarding_ruleset = optional(map(object({
+      name = string
+      rules = optional(map(object({
+        name = string
+        domain_name = string
+        state = optional(string, "Enabled")
+        destination_ip_addresses = map(string)
+      })))
+    })))
+  }))
+  description = <<DESCRIPTION
+A map of outbound endpoints to create on this resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+- `name` - The name for the endpoint
+- `subnet_id` - The subnet ID
+- `forwarding_ruleset` - (Optional) A map of forwarding rulesets to create on the outbound endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+  - `name` - The name of the forwarding ruleset
+  - `rules` - (Optional) A map of forwarding rules to create on the forwarding ruleset. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+    - `name` - The name of the forwarding rule
+    - `domain_name` - The domain name to forward
+    - `state` - (Optional) The state of the forwarding rule. Possible values are `Enabled` and `Disabled`. Defaults to `Enabled`.
+    - `destination_ip_addresses - a map of string, the key is the IP address and the value is the port
+DESCRIPTION
+# default = {
+#     "outbound1" = {
+#       name = "outbound1"
+#       subnet_id = "subid"
+#       forwarding_ruleset = {
+#         "ruleset1" = {
+#           name = "ruleset1"
+#           rules = {
+#             "rule1" = {
+#               name = "rule1"
+#               domain_name = "example.com"
+#               state = "Enabled"
+#               destination_ip_addresses = {
+#                 "10.1.1.1" = "53"
+#                 "10.1.1.2" = "53"
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#     "outbound2" = {
+#       name = "outbound2"
+#       subnet_id = "subid"
+#     }
+#   }
+}
