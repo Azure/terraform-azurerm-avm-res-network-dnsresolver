e2782a8df71b3f8bef761d563060f27a1d33f876
Itamar <64002130+humanascode@users.noreply.github.com>
2024-04-07 19:59:33 +0300

    Itamar/role assignments (#10)
    * SNFR15 - Category: Documentation - Automatic Documentation Generation
    
    * added role assignments for rulesets
    
    * tags,rbac,locks

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 
 locals {
+  location                           = var.location != null ? var.location : data.azurerm_resource_group.parent[0].location
   resource_group_location            = try(data.azurerm_resource_group.parent[0].location, null)
-  location                          = var.location != null ? var.location : data.azurerm_resource_group.parent[0].location
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,6 @@
-# TODO: insert outputs here.
-
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
-  value       = azurerm_private_dns_resolver.this
   description = "This is the full output for the resource."
+  value       = azurerm_private_dns_resolver.this
 }
