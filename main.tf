59c47f8bd75b09e6d9f418b36ea72c5ec96d045d
Itamar <itamarh@microsoft.com>
2024-04-10 17:43:48 +0300

    fixing pre-commit warnings

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_management_lock" "this" {
 
   lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_MY_RESOURCE.this.id
+  scope      = azurerm_private_dns_resolver.this.id
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
 }
 
