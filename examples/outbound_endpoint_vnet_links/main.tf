d090f10728461e1b59063b18391eb65930838042
Itamar <>
2024-06-17 17:58:55 +0300

    New example & version bump due to breaking change made by previous commit

---
+++ examples/outbound_endpoint_vnet_links/main.tf
@@ -0,0 +1,135 @@
+# This exmaple deploys a private DNS resolver with an inbound endpoint, two outbound endpoints, forwarding rulesets and rules, and aditional vnet links.
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    # TODO: Ensure all required providers are listed here.
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71.0, < 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+locals {
+  location = "northeurope"
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = local.location
+  name     = "rg-test-resolver-extended"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  address_space       = ["10.0.0.0/16"]
+  location            = local.location
+  name                = "vnet-test-resolver"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "name" {
+  address_prefixes     = ["10.0.0.0/24"]
+  name                 = "subnet-test-resolver-inbound"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+
+  lifecycle {
+    ignore_changes = [delegation]
+  }
+}
+
+resource "azurerm_subnet" "out" {
+  address_prefixes     = ["10.0.1.0/24"]
+  name                 = "subnet-test-resolver-outbound"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+
+  lifecycle {
+    ignore_changes = [delegation]
+  }
+}
+
+resource "azurerm_subnet" "out2" {
+  address_prefixes     = ["10.0.2.0/24"]
+  name                 = "subnet-test-resolver-outbound2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+
+  lifecycle {
+    ignore_changes = [delegation]
+  }
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  address_space       = ["10.90.0.0/16"]
+  location            = local.location
+  name                = "vnet-test-resolver2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+module "private_resolver" {
+  source = "../../" # Replace source with the following line
+  #source  = "Azure/avm-res-network-dnsresolver/azurerm"
+  resource_group_name         = azurerm_resource_group.rg.name
+  name                        = "resolver"
+  virtual_network_resource_id = azurerm_virtual_network.vnet1.id
+  location                    = local.location
+  inbound_endpoints = {
+    "inbound1" = {
+      name        = "inbound1"
+      subnet_name = azurerm_subnet.name.name
+
+    }
+  }
+  outbound_endpoints = {
+    "outbound1" = {
+      name        = "outbound1"
+      subnet_name = azurerm_subnet.out.name
+      forwarding_ruleset = {
+        "ruleset1" = {
+          name = "ruleset1"
+          additional_virtual_network_links = {
+            "vnet2" = {
+              id = azurerm_virtual_network.vnet2.id
+              metadata = {
+                "type" = "spoke"
+                "env"  = "dev"
+              }
+            }
+            rules = {
+              "rule1" = {
+                name        = "rule1"
+                domain_name = "example.com."
+                state       = "Enabled"
+                destination_ip_addresses = {
+                  "10.1.1.1" = "53"
+                  "10.1.1.2" = "53"
+                }
+              },
+              "rule2" = {
+                name        = "rule2"
+                domain_name = "example2.com."
+                state       = "Enabled"
+                destination_ip_addresses = {
+                  "10.2.2.2" = "53"
+                }
+              }
+            }
+          }
+        }
+      }
+      "outbound2" = {
+        name        = "outbound2"
+        subnet_name = azurerm_subnet.out2.name
+      }
+    }
+  }
+}
