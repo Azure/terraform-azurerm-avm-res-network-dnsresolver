95063c3e14ba397fdbf8b9ea05a18fb2cd4dd31b
Itamar <>
2024-10-13 20:21:14 +0300

    fixed provider config

---
+++ examples/default/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    # TODO: Ensure all required providers are listed here.
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71.0, < 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
