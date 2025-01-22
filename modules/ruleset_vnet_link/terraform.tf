b9fcdf3cc6fb37cc895a496af453b1675ce99cc9
Itamar <>
2025-01-22 18:13:55 +0200

    fix/remove_unneeded_modules

--- modules/ruleset_vnet_link/terraform.tf
+++ modules/ruleset_vnet_link/terraform.tf
@@ -5,13 +5,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    modtm = {
-      source  = "azure/modtm"
-      version = "~> 0.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0"
-    }
   }
 }
